652ce84ab5b35e0743dc7b8cd06017dfcbc7ac2e
akutsu masaya <masaya.akuthu@gmail.com>
2023-07-17 20:26:07 +0900

    image バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,7 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true
