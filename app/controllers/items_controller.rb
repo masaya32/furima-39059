4331351d3797b762af65cac3f98ae5589506325e
akutsu masaya <masaya.akuthu@gmail.com>
2023-07-27 21:17:02 +0900

    ユーザー確認の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,7 @@ class ItemsController < ApplicationController
 
   def destroy
      @item.destroy
+     redirect_to root_path
   end
 
   private
@@ -49,7 +50,7 @@ class ItemsController < ApplicationController
   end
 
   def confirm_user
-    redirect_to root_path unless current_user.id == @item
+    current_user.id == @item.user_id
   end
 
 end
