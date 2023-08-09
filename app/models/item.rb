41df377c332e7bd42ab191798eb36572af6567d8
akutsu masaya <masaya.akuthu@gmail.com>
2023-08-10 00:03:51 +0900

    プレビュー機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,6 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
-  has_one_attached :image
 
   validates :image, presence: true
   validates :name, presence: true
